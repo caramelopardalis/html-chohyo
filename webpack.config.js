72ffdee466446d6caad9fb55a6be0920f201f4fe
caramelopardalis <ltsu.koiwai@gmail.com>
2020-10-07 18:16:16 +0900

    Fix config files

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,7 @@
 module.exports = {
     mode: 'development',
     entry: {
-        'dist/html-chohyo': './src/js/html-chohyo.js',
-        'demo/dist/js/demo': './demo/src/js/demo.js'
+        'dist/html-chohyo': './src/js/html-chohyo.js'
     },
     output: {
         path: __dirname,
@@ -13,27 +12,7 @@ module.exports = {
                 test: /\.js$/,
                 use: [
                     {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: [
-                                '@babel/preset-env'
-                            ],
-                            plugins: [
-                                '@babel/plugin-transform-async-to-generator'
-                            ]
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            url: false
-                        }
+                        loader: 'babel-loader'
                     }
                 ]
             }
